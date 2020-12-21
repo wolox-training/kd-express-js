8bd1d68552a7260a7c17f432ce6f1d6ef436a134
Kevin De Ritis <kevin.deritis@wolox.com.ar>
2020-12-21 17:09:46 -0300

    Model user created

---
+++ app/models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: DataTypes.INTEGER,
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+    },
+    {}
+  );
+
+  return User;
+};
