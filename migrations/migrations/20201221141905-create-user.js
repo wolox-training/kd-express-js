c3951586677f7252a2690dd51721f05afaaeae6e
Kevin De Ritis <kevin.deritis@wolox.com.ar>
2020-12-29 18:44:59 -0300

    Migration changes, password added

--- migrations/migrations/20201221141905-create-user.js
+++ migrations/migrations/20201221141905-create-user.js
@@ -20,6 +20,10 @@ module.exports = {
         type: Sequelize.STRING,
         unique: true
       },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE
