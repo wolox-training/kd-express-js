7da85dc371e6c17193dd0adfe25260e107178c96
Kevin De Ritis <kevin.deritis@wolox.com.ar>
2020-12-30 16:40:12 -0300

    Creation of users test

--- test/users.test.js
+++ test/users.test.js
@@ -1,3 +1,19 @@
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
+const request = require('supertest');
+const app = require('../app');
+
+describe('User Creation', () => {
+  it('should create an user successfuly', async () => {
+    const user = {
+      id: 99,
+      firstName: 'User1',
+      lastName: 'LastnameUser1',
+      email: 'user99@wolox.com.ar',
+      password: 'contrasena22'
+    };
+    const res = await request(app)
+      .post('/users')
+      .send(user);
+    expect(res.status).toEqual(201);
+    expect(res.body.email).toEqual(user.email);
+  });
 });
