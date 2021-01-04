26f6f3920d350d4d9c8fb3470aea85113e038386
Kevin De Ritis <kevin.deritis@wolox.com.ar>
2021-01-04 14:45:18 -0300

    User controller changed

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,16 +1,9 @@
 const { signup } = require('../services/users');
-const error = require('../errors');
 const { crypt } = require('../helpers');
 
-exports.users = (req, res) => {
+exports.users = (req, res, next) => {
   req.body.password = crypt(req.body.password);
   signup(req.body)
-    .then(sign => {
-      if (sign.firstName) {
-        res.status(200).send(`User ${sign.firstName} created.`);
-      } else {
-        res.status(400).send(error.databaseError);
-      }
-    })
-    .catch(err => err);
+    .then(sign => res.status(200).send(sign))
+    .catch(next);
 };
